ca20a908b8914c4b12d93bf31bc3fb2bb4b0afd6
lifeisbeautiful <poltoradnevone@gmail.com>
2023-11-05 17:03:19 +0400

    feat: added cors

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 var conn = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApiDbContext>(options =>
-    options.UseNpgsql(conn));
+	options.UseNpgsql(conn));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,9 +19,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
+
+app.UseCors(options =>
+{
+	options.AllowAnyHeader().AllowAnyHeader().AllowAnyOrigin();
+});
 
 // app.UseHttpsRedirection();
 
