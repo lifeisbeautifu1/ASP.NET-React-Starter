9b2f8e157872da9da45d54b9753d4ebb30b39427
lifeisbeautiful <poltoradnevone@gmail.com>
2023-11-05 17:08:30 +0400

    hello?

---
+++ backend/Models/User.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+	[Table("users")]
+	public class User
+	{
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Column("name")]
+		public string Name { get; set; }
+
+		[Column("email")]
+		public string Email { get; set; }
+
+	}
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center text-5xl font-bold">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
